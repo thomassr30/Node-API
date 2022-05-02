7eb04dc38c38b79430ff7f962b915df631414e20
thomassr30 <thomassoto94@gmail.com>
2022-05-02 17:14:45 -0400

    importación de route storage

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {config} from "dotenv";
 import morgan from "morgan";
 import {dbConnect} from './config/db.js'
 import routerTracks from "./routes/tracks.js";
+import routerStorage from "./routes/storage.js";
 
 
 config()
@@ -13,9 +14,12 @@ const port = process.env.PORT
 
 app.use(cors())
 app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.static('storage'))
 
 // declare routes
 app.use('/API', routerTracks)
+app.use('/API', routerStorage)
 
 
 app.listen(port , () =>{
