d67ca2046f90932a56a5fcc53dc652b070b66265
thomassr30 <thomassoto94@gmail.com>
2022-05-03 15:44:56 -0400

    importación del validador

--- src/routes/tracks.js
+++ src/routes/tracks.js
@@ -6,12 +6,13 @@ import {
     updateItem,
     deleteItem
 } from '../controllers/tracks.js'
+import {validatorCreateItem} from '../validators/tracks.js'
 
 const router = Router()
 
 router.get('/tracks', getItems)
 router.get('/tracks/:id', getItem)
-router.post('/tracks', createItem)
+router.post('/tracks',validatorCreateItem , createItem)
 router.put('/tracks/:id', updateItem)
 router.delete('/tracks/id:', deleteItem)
 
