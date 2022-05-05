f15e50954defbe4e5a0b996fca5753f4dbc3749b
thomassr30 <thomassoto94@gmail.com>
2022-05-04 22:50:27 -0400

    middleware customHeader

--- src/routes/tracks.js
+++ src/routes/tracks.js
@@ -7,12 +7,13 @@ import {
     deleteItem
 } from '../controllers/tracks.js'
 import {validatorCreateItem} from '../validators/tracks.js'
+import {customHeader} from '../middleware/customHeader.js'
 
 const router = Router()
 
 router.get('/tracks', getItems)
 router.get('/tracks/:id', getItem)
-router.post('/tracks',validatorCreateItem , createItem)
+router.post('/tracks',validatorCreateItem, customHeader , createItem)
 router.put('/tracks/:id', updateItem)
 router.delete('/tracks/id:', deleteItem)
 
