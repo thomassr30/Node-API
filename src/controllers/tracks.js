e9e39f5c45f540274aafb6a6630c2ce8e1d92829
thomassr30 <thomassoto94@gmail.com>
2022-05-06 18:31:47 -0400

    controlador getItem y updateItem

--- src/controllers/tracks.js
+++ src/controllers/tracks.js
@@ -12,8 +12,15 @@ export const getItems = async (req, res) => {
     }
 }
 
-export const getItem = (req, res) => {
-
+export const getItem = async (req, res) => {
+    const {id} = req.params
+    try {
+        req = matchedData(req)
+        const data = await trackModel.findById(id)
+        res.send(data)
+    } catch (error) {
+        handleHttpError(res, 'ERROR_GET_ITEM')
+    }
 }
 
 export const createItem = async (req, res) => {
@@ -27,10 +34,16 @@ export const createItem = async (req, res) => {
     }
 }
 
-export const updateItem = (req, res) => {
-
+export const updateItem = async (req, res) => {
+    try {
+        const {id, ...body} = matchedData(req)
+        const data = await trackModel.findOneAndUpdate(id,body)
+        res.send({data})
+    } catch (error) {
+        handleHttpError(res, 'ERROR_UPDATE_ITEM')
+    }
 }
 
-export const deleteItem = (req, res) => {
+export const deleteItem = async (req, res) => {
 
 }
